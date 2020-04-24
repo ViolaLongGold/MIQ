eaec57830ecfb5ae2c9d315c7687a6164d85f976
Frank Höger <frank.hoeger@mirsem.org>
2020-04-24 21:57:42 +0200

    cleanup

--- data_raw/RAT_dict.R
+++
@@ -1,5 +0,0 @@
-MIQ_dict_raw <- readRDS("data_raw/MIQ_dict.RDS")
-names(MIQ_dict_raw) <- c("key", "DE", "EN")
-MIQ_dict_raw <- MIQ_dict_raw[,c("key", "EN", "DE")]
-MIQ_dict <- psychTestR::i18n_dict$new(MIQ_dict_raw)
-usethis::use_data(MIQ_dict, overwrite = TRUE)
