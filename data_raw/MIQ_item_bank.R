bb95c23f7d2a0629df247e0af799e43870ce2bc3
Frank Höger <frank.hoeger@mirsem.org>
2020-04-29 13:33:52 +0200

    fix column name for answer in item_bank

--- data_raw/MIQ_item_bank.R
+++ data_raw/MIQ_item_bank.R
@@ -2,7 +2,6 @@ item_bank <- readRDS("data_raw/MIQ_item_bank.RDS")
 item_bank$item_id <- sprintf("%s-%s", item_bank$pattern, item_bank$bit_flips)
 
 item_bank <- item_bank %>% distinct(item_id, .keep_all = T)
-item_bank$answer <- sample(1:4, nrow(item_bank), replace = T)
 item_bank$length.cat <- NULL
 item_bank$item_no <- NULL
 item_bank$training <- NULL
